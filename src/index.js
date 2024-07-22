9363d3d28f29948436a1730ab1e5d5fb5c132e14
Vipulpandey007 <vipulpandey708@gmail.com>
2024-07-22 15:59:33 +0530

    common middlewares added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("welcome");
+import dotenv from "dotenv";
+import { app } from "./app.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const PORT = process.env.PORT || 5003;
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});

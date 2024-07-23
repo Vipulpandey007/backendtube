d63edf60902e552b60a7a1679d19ed463e4e93bf
Vipulpandey007 <vipulpandey708@gmail.com>
2024-07-23 14:27:19 +0530

    healthcheck completed

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+//routes
+import healtcheckRouter from "./routes/healthcheck.routes.js";
 
 const app = express();
 
@@ -14,5 +16,7 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+
+app.use("/api/v1/healthcheck", healtcheckRouter);
 
 export { app };
---
+++ src/routes/healthcheck.routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { healthcheck } from "../controllers/healthcheckcontroller.js";
+
+const router = Router();
+
+router.route("/").get(healthcheck);
+
+export default router;
